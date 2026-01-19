50c87a8f45eeab1fe64b589f7f3ad5ee0eb2812a
anthony_ledes <anthony.ledes@outlook.com>
2026-01-18 20:12:47 -0500

    Modificacion de las interfaces, a mas de implemnetar losd metodos y condicionales dentro de la logica de negocio

---
+++ src/main/java/com/sistemabancario/sistemaBancario/repository/ICuentaBancariaRepository.java
@@ -0,0 +1,12 @@
+package com.sistemabancario.sistemaBancario.repository;
+
+import com.sistemabancario.sistemaBancario.model.CuentaBancaria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface CuentaBancariaRepository extends JpaRepository<CuentaBancaria, Long> {
+    //Buscar la cuenta por su número de cuenta de 10 digitos
+    Optional<CuentaBancaria> findByNumeroCuenta(String numeroCuenta);
+}
